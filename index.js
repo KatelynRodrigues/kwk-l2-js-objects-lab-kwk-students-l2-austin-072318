fa1406dd2a4eeb516cef122c4a85619211107c4d
Katelyn Rodrigues <katelyn.rodrigues1@gmail.com>
2018-07-27 14:19:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var driver = {}
+  
+
+function updateDriverWithKeyAndValue(driver, key, value){
+ let driver1 = driver
+ driver1[key] = value
+ return driver1
+}
+
+//updateDriverWithKeyAndValue("Sam", "address", "11 Broadway")
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key]=value
+  return driver
+}
+
+function deleteFromDriverByKey(driver,key){
+  let driver2 = driver
+  delete driver2[key]
+  console.log(driver)
+  return driver2
+}
+
+function destructivelyDeleteFromDriverByKey(driver,key){
+  delete driver[key]
+  return driver
+}
+  
+  
